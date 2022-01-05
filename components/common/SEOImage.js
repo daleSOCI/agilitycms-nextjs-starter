063085f7e3bc6ffc294cd5e4b4820a26459f43d4
dalespiteri <dalespiteri@gmail.com>
2022-01-05 16:25:05 -0500

    add SEO image component

---
+++ components/common/SEOImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SEOImage = ({ img, sizes, className }) => {
+
+    const srcSet = sizes.map(size => `${img?.url}?format=auto&w=${size} ${size}w, `);
+    return (
+        <>
+            {img && (
+                <img src={img.url} srcSet={srcSet} alt={img.label} className={className} />
+            )}
+        </>
+    );
+};
+
+export default SEOImage;
