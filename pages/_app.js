6ebd384a1f5b7f3df5460cb3e4e9e64c21722de9
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2022-01-06 13:56:25 -0500

    Added auth files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.scss';
 
+import useAuth from '../utils/hooks/useAuth';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const { loggedIn } = useAuth();
+
+  return ( 
+    <>
+      {loggedIn && <Component {...pageProps} />}
+      {!loggedIn && 'Redirecting to login page...'}
+    </>
+  );
 }
 
 export default MyApp
